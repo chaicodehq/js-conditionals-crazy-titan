ccfbfb450580b38e30f5a58a6f7da3ed80c9e1b9
Abhinav <abhinavspl2003@gmail.com>
2026-03-04 13:35:43 +0530

    add ticket pricing system

--- src/01-ticket-pricing.js
+++ src/01-ticket-pricing.js
@@ -23,5 +23,35 @@
  * @returns {number} The ticket price, or -1 for invalid input
  */
 export function getTicketPrice(age, isWeekend) {
-  // Your code here
+  let price;
+  if(age < 0 && typeof age != Number) return -1;
+
+  if(age >= 0 && age <= 12){
+    price = 8;
+    if(isWeekend){
+      return price = price + 3;
+    }
+    return price;
+  }
+  else if(age >= 13 && age <= 17){
+    price = 12;
+    if(isWeekend){
+      return price = price + 3;
+    }
+    return price;
+  }
+  if(age >= 18 && age <= 59){
+    price = 15;
+    if(isWeekend){
+      return price = price + 3;
+    }
+    return price;
+  }
+  else{
+    price = 10;
+    if(isWeekend){
+      return price = price + 3;
+    }
+    return price;
+  }
 }
